ce8d088a2fd9f589c05e75fdcc2b0184f50fe6a8
sheblair <66489366+sheblair@users.noreply.github.com>
2023-07-14 12:44:46 -0400

    mobile footer done

--- footer.php
+++ footer.php
@@ -17,14 +17,14 @@
 				<p>SAGA Gallery</p>
 				<p>32 Union Sq E, Suite 1214</p>
 				<p>New York, NY 10003</p>
-				<p>saga@sagaprints.com</p>
+				<p><a href="mailto:saga@sagaprints.com" class="footer-email-link">saga@sagaprints.com</a></p>
 			</div>
 			<div class="donate-button-wrapper">
 				<a href="https://www.paypal.com" target="_blank"><button class="donate-button">Donate</button></a>
 			</div>
 		</div>
 		<div class="footer-lower">
-			<!-- footer menu goes here -->
+			<?php wp_nav_menu( array( 'footer_menu' => 'footer-menu' ) ); ?>
 			<p class="copyright">© 2023 Society of American Graphic Artists</p>
 		</div>
 	</footer><!-- #colophon -->
--- functions.php
+++ functions.php
@@ -134,6 +134,12 @@ function saga_widgets_init() {
 }
 add_action( 'widgets_init', 'saga_widgets_init' );
 
+/* Register footer nav menu */
+function register_my_menu() {
+	register_nav_menu('footer-menu',__( 'Footer Menu' ));
+  }
+  add_action( 'init', 'register_my_menu' );
+
 /**
  * Enqueue scripts and styles.
  */
