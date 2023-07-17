bc428cba89e3993864f43ad5d4d954b9b1e51655
sheblair <66489366+sheblair@users.noreply.github.com>
2023-07-17 14:34:51 -0400

    add template part front-page

--- front-page.php
+++ front-page.php
@@ -11,17 +11,14 @@
 get_header();
 ?>
     <div class="accent-strip"></div>
+	
 	<main id="primary" class="site-main">
-		<?php
-		while ( have_posts() ) : the_post();
-
-			get_template_part( 'template-parts/content', 'page' );
-
-
-		endwhile; // End of the loop.
-		?>
-
-	</main><!-- #main -->
+		<?php while ( have_posts() ) : the_post(); ?>
+			<div class="front-page-posts">
+				<?php get_template_part( 'template-parts/content', 'front-page' ); ?>
+			</div>
+		<?php endwhile; ?>
+	</main>
 
 <?php
 get_footer();
