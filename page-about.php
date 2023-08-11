a53b1a3cf77367d124ce8b0f4964c2465479c391
sheblair <66489366+sheblair@users.noreply.github.com>
2023-08-11 15:04:17 -0400

    front-page and about-page blocks logic moved to respective pages

--- page-about.php
+++ page-about.php
@@ -26,7 +26,31 @@ get_header();
                 <?php the_content(); ?>
             </div>
             <div id="about-page-blocks" class="blocks-container">
-                <?php get_template_part( 'template-parts/content', 'about-page-blocks' ); ?>
+				<?php
+					$args = array( 
+						'post_type'      => 'post',
+						'posts_per_page' => -1, // -1 to display all posts, you can set a specific number if you prefer
+						'category_name' => 'about-page-block' 
+					);
+					
+					$loop = new WP_Query( $args );
+
+					if ( $loop -> have_posts() ) { 
+					
+					while ( $loop -> have_posts() ) { $loop -> the_post(); ?>
+
+						<div class="block about-page-block">
+							<?php the_post_thumbnail(); ?>
+							<?php the_content(); ?>
+						</div>
+
+				<?php }
+						wp_reset_postdata();
+
+					} else {
+						// No posts found with the specified tag
+						echo 'No posts found.';
+				} ?>
             </div>
 		</main>
 
