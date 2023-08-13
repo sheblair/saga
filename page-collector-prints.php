31997c50a3b4f00f57fb5552e33de039836b4dd1
sheblair <66489366+sheblair@users.noreply.github.com>
2023-08-13 14:15:45 -0400

    add collector prints page and style button

---
+++ page-collector-prints.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * The template for displaying the Collector Prints page
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package saga
+ */
+
+get_header();
+?>
+	<?php while ( have_posts() ) : the_post(); ?>
+
+		<div class="page-heading-wrapper">
+			<h1 class="page-heading"><?php the_title(); ?></h1>
+			<div class="accent-strip"></div>
+		</div>
+
+		<main id="collector-prints" class="site-main">
+			<?php get_template_part( 'template-parts/content', 'page' ); ?>
+		</main>
+
+	<?php endwhile; ?>
+
+<?php
+get_footer();
