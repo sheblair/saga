bfff0468cdd6f7d84b1396800958c17b494ec86c
sheblair <66489366+sheblair@users.noreply.github.com>
2023-08-31 10:16:04 -0400

    artist-blocks switch to artist_thumbnail ACF field name

--- template-parts/artist-blocks.php
+++ template-parts/artist-blocks.php
@@ -22,12 +22,12 @@
 
     if ( $loop->have_posts() ) { while ( $loop->have_posts() ) { $loop->the_post(); 
                 $name = get_field('name');
-                $thumbnail = get_field('thumbnail');
+                $artist_thumbnail = get_field('artist_thumbnail');
 				$size = 'full';
     ?>
     
         <div class="artist-block artist">
-            <a href="<?php the_permalink(); ?>"><figure class="artist-thumbnail"><?php echo wp_get_attachment_image( $thumbnail, $size ); ?></figure></a>
+            <a href="<?php the_permalink(); ?>"><figure class="artist-thumbnail"><?php echo wp_get_attachment_image( $artist_thumbnail, $size ); ?></figure></a>
             <a href="<?php the_permalink(); ?>"><h2 class="artist-name"><?php echo $name; ?></h2></a>
         </div>
 
